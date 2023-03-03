9561d190d4259e546dcada0aadc53e60e9b11ebe
mamenDseid <mamelala@gmail.com>
2023-03-03 15:12:05 +0000

    cdfj

--- test-shein/src/components/Nav.jsx
+++ test-shein/src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Solidbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
