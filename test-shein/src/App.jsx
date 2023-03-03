1105815e1653b6d94f530fc581caeb46067874d3
mamenDseid <mamelala@gmail.com>
2023-03-03 15:37:06 +0000

    cvfd

--- test-shein/src/App.jsx
+++ test-shein/src/App.jsx
@@ -1,16 +1,30 @@
-import { createSignal } from "solid-js";
+import { Routes } from "solid-app-router";
+import { createEffect, createSignal } from "solid-js";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
 
 
+/*
 
+const [users, setUsers]= createSignal([])
+createEffect(async()=>{
+const res = await fetch('https://jsonplaceholder.typicode.com/users')
+setUsers(res.json())
+})
+*/
  function App() {
+
 
   return (
     <div class="container">
       <Nav/>
-      <h1>Hello world</h1>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
+
+//export {users}
   export default App;
